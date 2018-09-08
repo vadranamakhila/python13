5b16304fa7c5e01940a3253bc326e50050f52b03
vadranamakhila <42572588+vadranamakhila@users.noreply.github.com>
2018-09-08 12:15:41 +0530

    Update even1.py

--- even1.py
+++ even1.py
@@ -1,5 +1,5 @@
-lower_limit=int(input("enter lower limit:"))
-upper_limit=int(input("enter upper limit:"))
+lower_limit=int(raw_input())
+upper_limit=int(raw_input())
 
 for i in range(lower_limit,upper_limit-1):
    if(i%2 == 0):
